6b3ed862cb2c904f869d821fc01630a877e682ae
Francisco Fuentes Schreiber <francisco.fuentes@alumnos.uv.cl>
2016-12-06 11:25:34 -0300

    lalaal

--- listener.js
+++ listener.js
@@ -1,10 +1,10 @@
-$(documents).ready(function)){
-	$('botoncito').click(
-		.get('../procesar.php' function(data){
-			alert("los datitos son"+data);
-
-
-
-			}
-
+$(document).ready(function(){
+	$('#botoncito').click(function(){
+		$.get('./procesar.php', function(data){
+			alert("Los datitos son: "+ data);
+				}
+			)
+		}
+	)
 }
+) 
